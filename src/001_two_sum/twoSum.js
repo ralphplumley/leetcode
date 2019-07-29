199d8e77beee3e1641f071f7eeb47e0eac0a644c
Ralph Plumley <ralphplumley@gmail.com>
2019-07-29 11:40:02 -0400

    Setup testing

---
+++ src/001_two_sum/twoSum.js
@@ -0,0 +1,15 @@
+const twoSum = (nums, target) => {
+    let map = new Map();
+  
+    for (let i = 0; i < nums.length; i++) {
+      let diff = target - nums[i];
+  
+      if (map.has(diff)) {
+        return [map.get(diff), i];
+      }
+  
+      map.set(nums[i], i);
+    }
+  };
+  
+  module.exports = twoSum;
